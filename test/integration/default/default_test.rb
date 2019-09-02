d468eb275d0a8ea7bd566bbcaf9b939060d71988
Alexandre <alexandre.franca@soprasteria.com>
2019-09-02 11:19:58 +0100

    AWS Recipe with Cloud IP Address

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,16 +1,19 @@
-# InSpec test for recipe workstation::default
+# # encoding: utf-8
 
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://www.inspec.io/docs/reference/resources/
+# Inspec test for recipe apache::default
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# The Inspec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec_reference.html
+
+describe port(80) do
+  it { should_not be_listening }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package('tree') do
+  it { should be_installed }
+end
+
+describe file('/etc/motd') do
+  its('content') { should match(/Property of/) }
+  it { should be_owned_by 'root' }
 end
